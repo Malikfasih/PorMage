a0eac0f5f9fadab56170deef5f5abf1535b80707
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-03-07 23:05:59 +0500

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProjectDetail from "./pages/ProjectDetail";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/project-detail/:id" exact element={<ProjectDetail />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
